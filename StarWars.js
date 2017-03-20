36271ba3a002b7495104898fb9c90c1c39375b7a
julietterighetti <juliette.righetti@devinci.fr>
2017-03-20 13:36:26 +0100

    WS 5
    StarWars.

---
+++ StarWars.js
@@ -0,0 +1,48 @@
+'use strict';
+
+ 
+var letters = [{
+  'author': '- Yoda',
+  'quote': 'When 900 years old, you reach… Look as good, you will not.',
+  'image': 'Yoda1.png'
+}, {
+  'author': '- Senator palpatine',
+  'quote': 'The Dark Side of the Force is the pathway to many abilities some consider to be.. Unnatural.',
+  'image': 'palpa.jpg'
+}, {
+  'author': '- Count Dooku',
+  'quote': 'I sense great fear in you, Skywalker. You have hate… you have anger… but you don’t use them.',
+  'image': 'DOOKU.png'
+}, {
+  'author': '- Darth vader',
+  'quote': 'Luke, you can destroy the Emperor. He has foreseen this. It is your destiny. Join me, and together we can rule the galaxy as father and son.',
+  'image': 'dark.jpg'
+}, {
+  'author': '- Princess leia',
+  'quote': 'Aren\'t you a little short for a storm trooper?',
+  'image': 'leia.jpg'
+}, {
+  'author': '- Yoda',
+  'quote': 'Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.',
+  'image': 'yoda2.jpg'
+}, {
+  'author': '- Qui-Gon Jinn',
+  'quote': 'Your focus determines your reality',
+  'image': 'qui gon.jpg'
+}, {
+  'author': '- Yoda',
+  'quote': 'Do. Or do not. There is no try.',
+  'image': 'Yoda3.png'
+}, {
+  'author': '- Obi-Wan-Kenobi',
+  'quote': 'In my experience there is no such thing as luck.',
+  'image': 'Masterobiwan.jpg'
+}, {
+  'author': '- Yoda',
+  'quote': 'If once you start down the dark side, forever will it dominate your destiney, consume you it will, as it did Obi-Wan\'s apprentice',
+  'image': 'Yoda1.png'
+}, {
+  'author': '- Darth Vader',
+  'quote': 'The Force is strong with this one.',
+  'image': 'dark.jpg'
+}];
